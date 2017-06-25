8ea5b54283ac4971a65aa5a2ef96a8981502e4d7
Walmir Neto <wfsneto@gmail.com>
2017-06-25 15:38:15 -0300

    add and configure apartment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  after_create :create_tenant
+
+  protected
+
+  def create_tenant
+    Apartment::Tenant.create(schema_name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,31 @@
-User.create(email: 'demo@comunika.com.br', schema_name: 'demo')
-User.create(email: 'visao@colegiovisaorecife.com.br', schema_name: 'visao')
-User.create(email: 'britanic@britanic.com.br', schema_name: 'britanic')
-User.create(email: 'colegiocasaforte@colegiocasaforte.com.br', schema_name: 'colegiocasaforte')
+User.create!(email: 'demo@comunika.com.br', schema_name: 'demo')
+
+Apartment::Tenant.switch!('demo')
+
+Project.create!(title: 'Projeto demo 1')
+Project.create!(title: 'Projeto demo 22')
+Project.create!(title: 'Projeto demo 333')
+
+User.create!(email: 'visao@colegiovisaorecife.com.br', schema_name: 'visao')
+
+Apartment::Tenant.switch!('visao')
+
+Project.create!(title: 'Projeto visao 1')
+Project.create!(title: 'Projeto visao 22')
+Project.create!(title: 'Projeto visao 333')
+
+User.create!(email: 'britanic@britanic.com.br', schema_name: 'britanic')
+
+Apartment::Tenant.switch!('britanic')
+
+Project.create!(title: 'Projeto britanic 1')
+Project.create!(title: 'Projeto britanic 22')
+Project.create!(title: 'Projeto britanic 333')
+
+User.create!(email: 'colegiocasaforte@colegiocasaforte.com.br', schema_name: 'colegiocasaforte')
+
+Apartment::Tenant.switch!('colegiocasaforte')
+
+Project.create!(title: 'Projeto colegiocasaforte 1')
+Project.create!(title: 'Projeto colegiocasaforte 22')
+Project.create!(title: 'Projeto colegiocasaforte 333')
