d973e1fa907841cfd839fe15a66820022a0098ab
Walmir Neto <wfsneto@gmail.com>
2017-06-25 13:36:01 -0300

    create users and projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'demo@comunika.com.br', schema_name: 'demo')
+User.create(email: 'visao@colegiovisaorecife.com.br', schema_name: 'visao')
+User.create(email: 'britanic@britanic.com.br', schema_name: 'britanic')
+User.create(email: 'colegiocasaforte@colegiocasaforte.com.br', schema_name: 'colegiocasaforte')
